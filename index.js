dac2430a8f4b10f88481835b781b930d4c806ef7
Jennifer Tchai <jentchai@gmail.com>
2021-12-14 22:42:54 -0500

    commit tree aa985355cfd0326f334c1b909e8733abe0a0eb12

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(something = "go to the office") {
+    return `This Monday, I will ${something}.`;
+}
+
+function wrapAdjective(flair = "*") {
+    return function innerFunction(param = "special") {
+    return `You are ${flair}${param}${flair}!`
+}
+    }
+wrapAdjective("||")("a dedicated programmer");
